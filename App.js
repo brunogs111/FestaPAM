5f52a2d5dfa3f5a8f93b29667aef3574e7ba0396
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-04-29 18:26:43 -0300

    comite

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Componente from './components/Componente';
 
 export default function App() {
+  const [num, setNum]= useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <View style={styles.row}>
+          <Text style={styles.texto}>Número de convidados</Text>
+          <TextInput style={styles.input} value={num} onChangeText={(value) => setNum(value)} />  
+        </View>
+        <Componente num={num}/>
     </View>
   );
 }
@@ -17,4 +23,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+    width: '100%'
+  },
+  input:{
+    width: '60%',
+    borderWidth: 1,
+    marginLeft: '20%',
+    marginTop: 50,
+    marginBottom: 50,
+    padding: 15,
+    fontSize: 20,
+    borderRadius: 6,
+  },
+  texto: {
+    fontSize: 30,
+    textAlign: 'center',
+  }
 });
---
+++ components/Componente.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+export default function Componente(props){
+    let texto = [];
+    let pessoa = [];
+    let refri = 600;    
+    let agua = 200;
+    let bolo = 100;
+    let doce = 3;
+    let salgadinho = 10;
+
+    texto.push(
+            <Text style = {styles.texto}>
+                Quantidade Pessoas: {(pessoa = parseFloat(props.num))} pessoas {'\n'}
+                Quantidade de refrigerante: {(refri * parseFloat(props.num))/1000} L {'\n'}
+                Quantidade de água: {(agua * parseFloat(props.num))/1000} L {'\n'}
+                Quantidade de bolo: {(bolo * parseFloat(props.num))/1000} Kg {'\n'}
+                Quantidade de doces: {doce * parseFloat(props.num)} unidades {'\n'}
+                Quantidade de salgadinhos: {salgadinho * parseFloat(props.num)} unidades {'\n'}
+            </Text>
+        );
+    
+    return(
+        <>
+            <View style = {styles.container}>
+                {texto}
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        fontFamily: 'Futura',
+        width: '40%',
+        padding: 18,
+        borderRadius: 15,  
+        backgroundColor: '#87CEEB',
+    },
+    texto: {
+        textAlign: "center",
+        fontSize: 26,
+    }
+});
